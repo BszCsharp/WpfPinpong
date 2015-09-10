ab0f68d0bb8479b89619579bb985c966b11a33ff
reinhold <reinhold@web.de>
2015-09-10 15:28:47 +0200

    Version1

---
+++ SpringBall/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SpringBall
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        System.Windows.Threading.DispatcherTimer timer = new System.Windows.Threading.DispatcherTimer();
+        double vx = 140.0;
+        double vy = 210.0;
+        int pleft = 0, pright = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer.Interval = TimeSpan.FromSeconds(0.05);
+            timer.Tick += animate;
+            timer.IsEnabled = true;
+            this.Title = pleft.ToString() + " : " + pright.ToString();
+
+        }
+
+        private void animate(object sender, EventArgs e)
+        {
+            Double x = Canvas.GetLeft(ball);
+            Double y = Canvas.GetTop(ball);
+            x += vx * timer.Interval.TotalSeconds;
+            // linker Schläger
+            if (x <= leftPaddle.Width && y >= Canvas.GetTop(leftPaddle) && y + ball.Height <= Canvas.GetTop(leftPaddle) + leftPaddle.Height)
+            {
+                vx  = - vx;
+                
+            }
+            // rechter Schläger
+            if (x + ball.Width  >= myCanvas.ActualWidth - rightPaddle.Width   && y >= Canvas.GetTop(rightPaddle) && y + ball.Height <= Canvas.GetTop(rightPaddle) + rightPaddle.Height)
+            {
+                vx = -vx;
+
+            }
+            Canvas.SetLeft(ball, x);
+            // Beinnpunkte
+            if (x <= 0)
+            {
+                pright += 1;
+                this.Title = pleft.ToString() + " : " + pright.ToString();
+                Canvas.SetLeft(ball, myCanvas.ActualWidth / 2.0);
+            }
+            if (x + ball.Width >= myCanvas.ActualWidth )
+            {
+                pleft += 1;
+                this.Title = pleft.ToString() + " : " + pright.ToString();
+                Canvas.SetLeft(ball, myCanvas.ActualWidth / 2.0);
+            }
+
+            //End
+            y += vy * timer.Interval.TotalSeconds;
+            if(y <= 0.0  || y >= myCanvas.ActualHeight  - ball.Height)
+            {
+                vy = -vy;
+            }
+            Canvas.SetTop(ball, y);
+
+        }
+
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point p = e.GetPosition(this);
+            //this.Title = p.X.ToString();
+            Canvas.SetTop(leftPaddle, p.Y);
+            Canvas.SetTop(rightPaddle, p.Y);
+        }
+    }
+}
---
+++ SpringBall/Pingpong.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SpringBall
+{
+    /// <summary>
+    /// Interaktionslogik für Pingpong.xaml
+    /// </summary>
+    public partial class Pingpong : Window
+    {
+        System.Windows.Threading.DispatcherTimer timer = new System.Windows.Threading.DispatcherTimer();
+        double vx = 140.0;
+        double vy = 210.0;
+        int pleft = 0, pright = 0;
+        public Pingpong()
+        {
+            InitializeComponent();
+            timer.Interval = TimeSpan.FromSeconds(0.05);
+            timer.Tick += ticker;
+            timer.IsEnabled = true;
+        }
+
+        private void ticker(object sender, EventArgs e)
+        {
+            Double x = Canvas.GetLeft(ball);
+            Double y = Canvas.GetTop(ball);
+            x += vx * timer.Interval.TotalSeconds;
+            // linker Schläger
+            if (x <= leftPaddle.Width && y >= Canvas.GetTop(leftPaddle) && y + ball.Height <= Canvas.GetTop(leftPaddle) + leftPaddle.Height)
+            {
+                vx = -vx;
+
+            }
+            // rechter Schläger
+            if (x + ball.Width >= myCanvas.ActualWidth - rightPaddle.Width && y >= Canvas.GetTop(rightPaddle) && y + ball.Height <= Canvas.GetTop(rightPaddle) + rightPaddle.Height)
+            {
+                vx = -vx;
+
+            }
+            Canvas.SetLeft(ball, x);
+            // Punkte
+            if (x <= 0)
+            {
+                pright += 1;
+                this.Title = pleft.ToString() + " : " + pright.ToString();
+                Canvas.SetLeft(ball, myCanvas.ActualWidth / 2.0);
+                vx *= 1.1;
+            }
+            if (x + ball.Width >= myCanvas.ActualWidth)
+            {
+                pleft += 1;
+                this.Title = pleft.ToString() + " : " + pright.ToString();
+                Canvas.SetLeft(ball, myCanvas.ActualWidth / 2.0);
+                vx *= 1.1;
+            }
+
+
+            //Enpunkte
+            y += vy * timer.Interval.TotalSeconds;
+            if (y <= 0.0 || y >= myCanvas.ActualHeight - ball.Height)
+            {
+                vy = -vy;
+            }
+            Canvas.SetTop(ball, y);
+
+        }
+
+  
+       private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point p = e.GetPosition(this);
+            Canvas.SetTop(leftPaddle, p.Y);
+            Canvas.SetTop(rightPaddle, p.Y);
+        }
+
+       private void Window_Loaded(object sender, RoutedEventArgs e)
+       {
+           BitmapImage bitmap = new BitmapImage();
+           bitmap.BeginInit();
+           bitmap.UriSource = new Uri("screen.png", UriKind.Relative);
+           bitmap.EndInit();
+           MyImage.ImageSource = bitmap;
+           // Test
+       }
+        
+    }
+}
